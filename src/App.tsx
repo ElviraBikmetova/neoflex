8946bdba09779fbf35c93468bbfe2fe1e800279d
Elvira <gaellru@gmail.com>
2024-04-01 04:22:52 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { ERoutes } from "./enums/routes";
 import CartPage from "./pages/CartPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import Layout from "./layout/Layout";
 import HomePage from "./pages/HomePage";
+import { useEffect } from "react";
 
 function App() {
+  const navigate = useNavigate()
+  useEffect(() => {
+    navigate(ERoutes.Root)
+  }, [])
   return (
     <Routes>
       <Route path={ERoutes.Root} element={<Layout />}>
